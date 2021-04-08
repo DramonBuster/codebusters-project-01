1ffd51fea0cceb4d09ed79e25b4b46e4f07470b0
Maria Kononchuk <mariyka.kononchuk@gmail.com>
2021-04-08 17:22:03 +0300

    добавила модалку в конткты

--- src/modal2.js
+++ src/modal2.js
@@ -1,8 +1,8 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
-    closeModalBtn: document.querySelector('[data-modal-close]'),
-    modal: document.querySelector('[data-modal]'),
+    openModalBtn: document.querySelector('[bata-modal-open]'),
+    closeModalBtn: document.querySelector('[bata-modal-close]'),
+    modal: document.querySelector('[bata-modal]'),
   };
 
   refs.openModalBtn.addEventListener('click', toggleModal);
